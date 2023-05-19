ae424bf8e92ba4d9ee492129891ec11f4c1111a3
Abdi Majid <abdi.amajid.dev>
2023-05-19 12:14:56 +0100

    modal created

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PagesList } from './PagesList';
+import { Page } from './Page';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,6 +8,7 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [selectedPage, setSelectedPage] = useState(null);
 
 	async function fetchPages(){
 		try {
@@ -22,12 +24,21 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	const handleBackToWikiClick = () => {
+		setSelectedPage(null);
+	  };
+
 	return (
 		<main className=''>	
       <h1>WikiVerse</h1>
 	  <button>Add Article</button>
 	  <div className='mainContainer'>
-			<PagesList pages={pages} />
+	  <PagesList pages={pages} setSelectedPage={setSelectedPage} />
+        {selectedPage && (
+    <Page page={selectedPage}
+            handleBackToWikiClick={handleBackToWikiClick}
+          />
+        )}
 			</div>
 		</main>
 	)
